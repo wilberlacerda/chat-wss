b7ab6a41a086b36d59a4a136b0582bb1c20ca9ff
Wilber Lacerda <wilberlacerda@gmail.com>
2020-04-07 15:37:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
+
+app.use(express.static("public"))
+
 const http = require('http').Server(app)
 const serverSocket = require('socket.io')(http)
-const porta = 8000
+
+const porta = process.env.PORT || 8000
+
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
 
 http.listen(porta, function(){
-    console.log('Servidor iniciado. Abra o navegador em http://localhost:'+porta);
+    const portaStr = porta === 80 ? '' :  ':' + porta
+
+    if (process.env.HEROKU_APP_NAME) 
+        console.log('Servidor iniciado. Abra o navegador em ' + host)
+    else console.log('Servidor iniciado. Abra o navegador em ' + host + portaStr)
 })
 
-app.get('/', function(req, resp){
-    resp.sendFile(__dirname + '/index.html')
+app.get('/', function (requisicao, resposta) {
+    resposta.sendFile(__dirname + '/index.html')
 })
+
 
 serverSocket.on('connection', function(socket){
     
@@ -23,7 +33,7 @@ serverSocket.on('connection', function(socket){
 
     socket.on('disconnect', function(){
         console.log('Cliente desconectado: ' + socket.nickname)
-        serverSocket.emit('chat-msg',  socket.nickname + ' saiu') /*Informa as outros usuarios, que um cliente desconectou*/
+        serverSocket.emit('chat-msg',  socket.nickname + ' saiu') 
     })
 
     socket.on('chat-msg', function(msg) {
